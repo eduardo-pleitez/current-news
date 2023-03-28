5473f1132d6e4079eccb8dc66c6de54000ca9daa
Eduardo Mejia Pleitez <eduardo.77.1996@gmail.com>
2023-03-27 20:14:45 -0600

    :sparkles: Add comments page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Comments from './pages/Comments';
 import './App.css';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/comments' element={<Comments />} />
     </Routes>
   );
 }
