0d7e567eccb795d27699608fbe12b1c0c1195b94
Eduardo Mejia Pleitez <eduardo.77.1996@gmail.com>
2023-03-28 18:04:49 -0600

    :sparkles: Update NewsList

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -6,13 +6,15 @@ import Col from 'react-bootstrap/Col';
 export default function NewsList({ data }) {
   return (
     <Container className='mt-3'>
-      <Row xs={1} md={2} lg={3} className="justify-content-center">
+      <Row xs={1} md={2} lg={3} className='justify-content-center'>
       {data.map((newsItem, index) => {
-        return (<Col key={index} >
-          <div className="mt-4 px-2">
-            <img className='rounded' src={newsItem.media} alt={newsItem.title} />
-            <p className='p-2 bg-primary text-white rounded'>{newsItem.title}</p>
-          </div>
+        return (<Col key={index} title='Go to this article'>
+          <a href={newsItem.link}>
+            <div className='mt-4 px-2'>
+              <img className='rounded' src={newsItem.media} alt={newsItem.title} />
+              <p className='p-2 bg-primary text-white rounded'>{newsItem.title}</p>
+            </div>
+          </a>
         </Col>)
       })}
       </Row>
