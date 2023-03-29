3c11470f1bc75579bba4e47478f13541c42f7803
Eduardo Mejia Pleitez <eduardo.77.1996@gmail.com>
2023-03-28 19:16:17 -0600

    :sparkles: Add target in NewsList

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -9,7 +9,7 @@ export default function NewsList({ data }) {
       <Row xs={1} md={2} lg={3} className='justify-content-center'>
       {data.map((newsItem, index) => {
         return (<Col key={index} title='Go to this article'>
-          <a href={newsItem.link}>
+          <a href={newsItem.link} target="_blank" rel="noreferrer">
             <div className='mt-4 px-2'>
               <img className='rounded' src={newsItem.media} alt={newsItem.title} />
               <p className='p-2 bg-primary text-white rounded'>{newsItem.title}</p>
